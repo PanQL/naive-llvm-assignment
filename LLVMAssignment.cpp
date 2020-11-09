49c14331cf3b1fd833890b7299a484e2546fb677
PanQL <panqinglin00@163.com>
2020-11-09 18:59:37 +0800

    pass some case, about 3

--- LLVMAssignment.cpp
+++ LLVMAssignment.cpp
@@ -25,6 +25,9 @@
 #include <llvm/Support/raw_ostream.h>
 #include <llvm/Transforms/Scalar.h>
 #include <llvm/Transforms/Utils.h>
+#include <llvm/IR/Instructions.h>
+#include <list>
+#include <map>
 
 using namespace llvm;
 static ManagedStatic<LLVMContext> GlobalContext;
@@ -33,19 +36,17 @@ static LLVMContext &getGlobalContext() { return *GlobalContext; }
  * will have optnone attribute which would lead to some transform passes
  * disabled, like mem2reg.
  */
-struct EnableFunctionOptPass : public FunctionPass {
+struct EnableFunctionOptPass: public FunctionPass {
     static char ID;
-    EnableFunctionOptPass() : FunctionPass(ID) {}
-    bool runOnFunction(Function &F) override {
-        errs() << "Hello panpan: ";
-        errs().write_escaped(F.getName()) << '\n';
-        if (F.hasFnAttribute(Attribute::OptimizeNone)) {
+    EnableFunctionOptPass():FunctionPass(ID){}
+    bool runOnFunction(Function & F) override{
+        if(F.hasFnAttribute(Attribute::OptimizeNone))
+        {
             F.removeFnAttr(Attribute::OptimizeNone);
         }
         return true;
     }
 };
-
 char EnableFunctionOptPass::ID = 0;
 
 ///!TODO TO BE COMPLETED BY YOU FOR ASSIGNMENT 2
@@ -55,13 +56,51 @@ struct FuncPtrPass : public ModulePass {
     static char ID;  // Pass identification, replacement for typeid
     FuncPtrPass() : ModulePass(ID) {}
 
+	int line = 0;
+	std::list<std::string> names;
+	std::map<int, std::list<std::string>> result;
+
     bool runOnModule(Module &M) override {
-        errs() << "Hello: ";
-        errs().write_escaped(M.getName()) << '\n';
-        M.dump();
-        errs() << "------------------------------\n";
+		bool res = false;
+		Module *tmp = &M;
+		for (Module::iterator ff = tmp->begin(); ff != tmp->end(); ++ff)
+			for (Function::iterator bb = ff->begin(); bb != ff->end(); ++bb)
+				for (BasicBlock::iterator ii = bb->begin(); ii != bb->end(); ++ii)
+					res |= checkCallInst(dyn_cast<CallInst>(ii));
+		for (std::map<int, std::list<std::string>>::iterator it = result.begin(); it != result.end(); ++it){
+			errs() << it->first << " : ";
+			for (std::list<std::string>::iterator name = it->second.begin(); name != it->second.end(); ++name)
+				errs() << *name << ' ';
+			errs() << '\n';
+		}
         return false;
     }
+
+	bool checkCallInst(CallInst *callInst) {
+		if(!callInst)return false;
+		this->line = callInst->getDebugLoc().getLine();
+		if(Function *func = callInst->getCalledFunction()){	// 最简单的直接函数调用
+			names.push_back(func->getName());
+		}else if(Value *value = callInst->getCalledOperand()){
+			checkPhiNode(dyn_cast<PHINode>(value));
+		}
+		result[this->line] = std::move(this->names);
+		return false;
+	}
+
+	void checkPhiNode(PHINode *node) {
+		if(!node)return;
+		auto range = node->incoming_values();
+		for (PHINode::op_iterator it = range.begin(); it != range.end(); ++it)
+			if (Function *funcInPhi = dyn_cast<Function>(it->get()))
+				names.push_back(funcInPhi->getName());
+			else if(PHINode *newnode = dyn_cast<PHINode>(it->get()))
+				checkPhiNode(newnode);
+			else if(Argument *arg = dyn_cast<Argument>(it->get()))
+				errs() << "still unimplement it !\n";
+			else
+				errs() << "somethin you never take account\n";
+	}
 };
 
 char FuncPtrPass::ID = 0;
